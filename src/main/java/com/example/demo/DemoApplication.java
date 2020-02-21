cae451ed5024282422d1d09ddece054503377a9c
BuRNIngsuperman <2502975747@qq.com>
2020-02-21 20:39:46 +0800

    update 1.0

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @ SpringBootApplication来标注一个主程序
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.example.demo.mapper"})
-public class DemoApplication {
+public class DemoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/controller/DruidController.java
+++ src/main/java/com/example/demo/controller/DruidController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/druid")
 public class DruidController {
 
     @Autowired
